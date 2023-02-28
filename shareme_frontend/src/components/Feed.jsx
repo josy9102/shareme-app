13817f4035358699cb50c64b500eaf171470b5bd
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-02-28 15:58:54 +0100

    started working on retrieving the pin and showing it on the feed

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-function Feed() {
-  return (
-    <div>Feed</div>
-  )
-}
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
-export default Feed
+const Feed = () => {
+  const [loading, setloading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+  useEffect(() => {
+    setloading(true);
+
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setloading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setloading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed!" />;
+
+  return <div> {pins && <MasonryLayout pins={pins} />} </div>;
+};
+
+export default Feed;
---
+++ shareme_frontend/src/components/Pin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { urlFor } from "../client";
+
+const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  return (
+    <div>
+      <img
+        src={urlFor(image).width(250).url()}
+        alt="user-post"
+        className="rounded-lg w-full"
+      />
+    </div>
+  );
+};
+
+export default Pin;
