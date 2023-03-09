be965933150441a544fad8b424c80dda658e1ea0
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-03-09 14:25:27 +0100

    search implemented

--- shareme_frontend/src/components/Search.jsx
+++ shareme_frontend/src/components/Search.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Search({ searchTerm, setSearchTerm }) {
-  return <div>Search</div>;
+import MasonryLayout from "./MasonryLayout";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+function Search({ searchTerm }) {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase())
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching for pins" />}
+      {pins?.length > 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No pins found</div>
+      )}
+    </div>
+  );
 }
 
 export default Search;
