0ce91f1d24dcf87fa2b58edb90e2cf3d34e790b9
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-03-09 14:17:53 +0100

    user profile page finished

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
+import { categories } from "../utils/data";
 
 import logo from "../assets/logo.png";
 
@@ -9,14 +10,6 @@ const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  { name: "Animals" },
-  { name: "Wallpapers" },
-  { name: "Photography" },
-  { name: "Sports" },
-  { name: "Coding" },
-];
 
 function Sidebar({ user, closeToggle }) {
   const handleCloseSidebar = () => {
@@ -56,6 +49,11 @@ function Sidebar({ user, closeToggle }) {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              />
               {category.name}
             </NavLink>
           ))}
